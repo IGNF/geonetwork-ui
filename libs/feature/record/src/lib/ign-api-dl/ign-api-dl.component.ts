3b4be88edd6714588b9ad570f239a329c2ca9548
Axel Losco <axel.losco@ign.fr>
2024-05-21 16:04:10 +0200

    Add( mise en forme des produits télécharable)

--- libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
+++ libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
@@ -69,13 +69,13 @@ export class IgnApiDlComponent implements OnInit {
   editionDate$ = new BehaviorSubject('')
   zone$ = new BehaviorSubject('')
   format$ = new BehaviorSubject('')
-  spatialDatasetIdentifierCode$ = new BehaviorSubject('')
+  category$ = new BehaviorSubject('')
 
 
   choicesFormat$: Observable<Choice[]>
   choicesZone$: Observable<Choice[]>
   choicesEditionDate$: Observable<Choice[]>
-  choicesSpatialDatasetIdentifierCode$: Observable<Choice[]>
+  choicesCategory$: Observable<Choice[]>
   // listProduit$: Observable<any>
   // subProducts$: Observable <Array<string>>
 
@@ -88,16 +88,15 @@ export class IgnApiDlComponent implements OnInit {
     this.choicesZone$ = this.getFields('BDORTHO', 'zone');
     this.choicesFormat$ = this.getFields('BDORTHO', 'format');
     this.choicesEditionDate$ = this.getFields('BDORTHO', 'editionDate')
-    this.choicesSpatialDatasetIdentifierCode$ = this.getFields('BDORTHO', 'spatialDatasetIdentifierCode')
+    this.choicesCategory$ = this.getFields('BDORTHO', 'category')
   }
 
-  apiQueryUrl$ = combineLatest([this.zone$, this.format$,this.editionDate$,this.spatialDatasetIdentifierCode$]).pipe(
-    map(([zone, format, editionDate,spatialDatasetIdentifierCode]) => {
-      console.log('on change de valeur')
+  apiQueryUrl$ = combineLatest([this.zone$, this.format$,this.editionDate$,this.category$]).pipe(
+    map(([zone, format, editionDate,category]) => {
       let outputUrl
       if (this.apiBaseUrl) {
         const url = new URL(this.apiBaseUrl) // initialisation de l'url avec l'url de base
-        const params = { zone: zone, format: format, editionDate: editionDate ,spatialDatasetIdentifierCode: spatialDatasetIdentifierCode} // initialisation des paramètres de filtres
+        const params = { zone: zone, format: format, editionDate: editionDate ,crs: category} // initialisation des paramètres de filtres
         for (const [key, value] of Object.entries(params)) {
           if (value && value !== '0') {
             url.searchParams.set(key, value)
@@ -120,9 +119,13 @@ export class IgnApiDlComponent implements OnInit {
 
   getProduits(url): Observable<Array<any>> {
     return this.http.get(url).pipe(
-      map((response) => response['entry'].map((el) => el['id'])),
+      map((response) => response['entry']),
       tap((el) => console.log(el))
     )
+  }
+  getLinkFormat(produit):string{
+    //console.log(produit['format'][0])
+    return produit['format'][0]['label']
   }
   // getSubProduct(url: string) {
   //   return this.http
@@ -166,8 +169,8 @@ export class IgnApiDlComponent implements OnInit {
     this.zone$.next(value)
   }
 
-  setSpatialDatasetIdentifierCode(value: string) {
-    this.spatialDatasetIdentifierCode$.next(value)
+  setCategory(value: string) {
+    this.category$.next(value)
   }
 
 
@@ -179,6 +182,7 @@ export class IgnApiDlComponent implements OnInit {
     // this.offset$.next(DEFAULT_PARAMS.OFFSET)
     this.zone$.next(this.choicesZone$[0])
     this.format$.next(this.choicesFormat$[0])
+    this.category$.next(this.choicesFormat$[0])
   }
 
   // getProduit(response){
@@ -203,25 +207,18 @@ export class IgnApiDlComponent implements OnInit {
             'https://data.geopf.fr/telechargement/resource/'.concat(produit)
         )[0]
       ),
-      tap((el) => console.log(el)),
+      //tap((el) => console.log(el)),
       switchMap((buckets: FormatProduit) => {
-       if (param != 'editionDate' && param != 'spatialDatasetIdentifierCode' ) {
-        console.log(typeof(buckets[param]));
-        const bucketPromises = buckets[param].map((bucket) => ({
+        //console.log(buckets)
+        const bucketPromises = buckets[param].map((bucket) => (
+          {
           value: bucket.label,
-          label: bucket.term || param,
+          label: bucket.term
         }))
+        bucketPromises.unshift({value: "",label: param});
+        console.log(bucketPromises);
         return Promise.all(bucketPromises)}
-        else{
-          console.log(typeof(buckets[param]));
-          const bucketPromises = [{
-            value: buckets[param] ||"",
-            label: buckets[param] || param
-          }]
-          console.log(bucketPromises);
-          return Promise.all(bucketPromises)
-        }
-      })
+      )
     )
   }
 
