98840a709096e938c4f2568140c8d389f204740b
mmohad <melodia.mohad@ign.fr>
2024-05-27 18:08:54 +0200

    fix(style): revert style

--- libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
+++ libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
@@ -63,19 +63,18 @@ export interface Field {
 export class IgnApiDlComponent implements OnInit {
   @Input() set apiLink(value: DatasetServiceDistribution) {
     this.apiBaseUrl = value ? value.url.href : undefined
-    console.log(this.apiBaseUrl)
     this.resetUrl()
   }
 
   editionDate$ = new BehaviorSubject('')
   zone$ = new BehaviorSubject('')
   format$ = new BehaviorSubject('')
-  spatialDatasetIdentifierCode$ = new BehaviorSubject('')
+  category$ = new BehaviorSubject('')
 
   choicesFormat$: Observable<Choice[]>
   choicesZone$: Observable<Choice[]>
   choicesEditionDate$: Observable<Choice[]>
-  choicesSpatialDatasetIdentifierCode$: Observable<Choice[]>
+  choicesCategory$: Observable<Choice[]>
   // listProduit$: Observable<any>
   // subProducts$: Observable <Array<string>>
 
@@ -85,41 +84,38 @@ export class IgnApiDlComponent implements OnInit {
   apiBaseUrl: string
 
   ngOnInit(): void {
-    this.choicesZone$ = this.getFields('zone')
-    this.choicesFormat$ = this.getFields('format')
-    this.choicesEditionDate$ = this.getFields('editionDate')
-    this.choicesSpatialDatasetIdentifierCode$ = this.getFields(
-      'spatialDatasetIdentifierCode'
-    )
+    this.choicesZone$ = this.getFields('BDORTHO', 'zone')
+    this.choicesFormat$ = this.getFields('BDORTHO', 'format')
+    this.choicesEditionDate$ = this.getFields('BDORTHO', 'editionDate')
+    this.choicesCategory$ = this.getFields('BDORTHO', 'category')
   }
 
   apiQueryUrl$ = combineLatest([
     this.zone$,
     this.format$,
     this.editionDate$,
-    this.spatialDatasetIdentifierCode$,
+    this.category$,
   ]).pipe(
-    map(([zone, format, editionDate, spatialDatasetIdentifierCode]) => {
-      console.log('on change de valeur')
-      if (!this.apiBaseUrl) {
-        return null
-      }
-      const url = new URL(this.apiBaseUrl) // initialisation de l'url avec l'url de base
-      const params = {
-        zone: zone,
-        format: format,
-        editionDate: editionDate,
-        spatialDatasetIdentifierCode: spatialDatasetIdentifierCode,
-      } // initialisation des paramètres de filtres
-      for (const [key, value] of Object.entries(params)) {
-        if (value && value !== '0') {
-          url.searchParams.set(key, value)
-        } else {
-          url.searchParams.delete(key)
+    map(([zone, format, editionDate, category]) => {
+      let outputUrl
+      if (this.apiBaseUrl) {
+        const url = new URL(this.apiBaseUrl) // initialisation de l'url avec l'url de base
+        const params = {
+          zone: zone,
+          format: format,
+          editionDate: editionDate,
+          crs: category,
+        } // initialisation des paramètres de filtres
+        for (const [key, value] of Object.entries(params)) {
+          if (value && value !== '0') {
+            url.searchParams.set(key, value)
+          } else {
+            url.searchParams.delete(key)
+          }
         }
+        outputUrl = url.toString()
+        console.log(outputUrl)
       }
-      let outputUrl = url.toString()
-      console.log(outputUrl)
       return outputUrl
     })
   )
@@ -132,10 +128,47 @@ export class IgnApiDlComponent implements OnInit {
 
   getProduits(url): Observable<Array<any>> {
     return this.http.get(url).pipe(
-      map((response) => response['entry'].map((el) => el['id'])),
+      map((response) => response['entry']),
       tap((el) => console.log(el))
     )
   }
+  getLinkFormat(produit): string {
+    //console.log(produit['format'][0])
+    return produit['format'][0]['label']
+  }
+  // getSubProduct(url: string) {
+  //   return this.http
+  //     .get(url, { headers: { accept: 'application/json' } })
+  //     .pipe(map((response) => response['entry'].map((el) => el['id'])))
+  // }
+
+  // getDownloadLink(url: string) {
+  //   return this.http
+  //     .get(url, { headers: { accept: 'application/json' } })
+  //     .pipe(map((response) => response['entry']))
+  // }
+  // getProduct(){
+  //   return this.apiQueryUrl$.pipe(
+  //     concatMap(url=> this.getSubProduct(url).pipe(
+  //       map((link) => {
+  //         const element = [] as Array<string>
+  //         for (let indexLink = 0; indexLink < link.length; indexLink++) {
+  //           const elementToAdd = this.getDownloadLink(link[indexLink]).subscribe(
+  //             // eslint-disable-next-line prefer-spread
+  //             (linkDownload) => element.push.apply(element, linkDownload)
+  //           )
+  //         }
+  //         console.log("sortie de requete produit : ",element)
+  //         return element
+  //       })
+  //     )))
+  // }
+
+  // getClassForFilter(index: number) {
+  //   return (
+  //     (this.isOpen ? 'block' : 'hidden') + ' ' + (index < 2 ? 'sm:block' : '')
+  //   )
+  // }
 
   setEditionDate(value: string) {
     this.editionDate$.next(value)
@@ -145,8 +178,8 @@ export class IgnApiDlComponent implements OnInit {
     this.zone$.next(value)
   }
 
-  setSpatialDatasetIdentifierCode(value: string) {
-    this.spatialDatasetIdentifierCode$.next(value)
+  setCategory(value: string) {
+    this.category$.next(value)
   }
 
   setFormat(value: string) {
@@ -154,41 +187,67 @@ export class IgnApiDlComponent implements OnInit {
   }
 
   resetUrl() {
+    // this.offset$.next(DEFAULT_PARAMS.OFFSET)
     this.zone$.next(this.choicesZone$[0])
     this.format$.next(this.choicesFormat$[0])
+    this.category$.next(this.choicesFormat$[0])
   }
+
+  // getProduit(response){
+
+  //   console.log('produit dans le html',response)
+  // }
+
+  // rechercheProduit(){
+  //   console.log('bouton apuyer');
+  //   //this.zone$.next(this.zoneFiltree)
+  //   this.format$.next(this.formatFiltree)
+  // }
 
   url = 'https://data.geopf.fr/telechargement/capabilities'
 
-  getFields(param: string): Observable<FieldAvailableValue[]> {
+  getFields(produit: string, param: string): Observable<FieldAvailableValue[]> {
     return this.http.get(this.url).pipe(
       map(
         (response) =>
           (response as Field).entry.filter(
-            (element) => element['id'] == this.apiBaseUrl
+            (element) =>
+              element['id'] ==
+              'https://data.geopf.fr/telechargement/resource/'.concat(produit)
           )[0]
       ),
-      tap((el) => console.log(el)),
+      //tap((el) => console.log(el)),
       switchMap((buckets: FormatProduit) => {
-        if (param != 'editionDate' && param != 'spatialDatasetIdentifierCode') {
-          console.log(typeof buckets[param])
-          const bucketPromises = buckets[param].map((bucket) => ({
-            value: bucket.label,
-            label: bucket.term || param,
-          }))
-          return Promise.all(bucketPromises)
-        } else {
-          console.log(typeof buckets[param])
-          const bucketPromises = [
-            {
-              value: buckets[param] || '',
-              label: buckets[param] || param,
-            },
-          ]
-          console.log(bucketPromises)
-          return Promise.all(bucketPromises)
-        }
+        //console.log(buckets)
+        const bucketPromises = buckets[param].map((bucket) => ({
+          value: bucket.label,
+          label: bucket.term,
+        }))
+        bucketPromises.unshift({ value: '', label: param })
+        console.log(bucketPromises)
+        return Promise.all(bucketPromises)
       })
     )
   }
+
+  // getFieldsUnique(produit: string, param: string): Observable<FieldAvailableValue[]> {
+  //   return this.http.get(this.url).pipe(
+  //     map((response) =>
+  //       (response as Field).entry.filter(
+  //         (element) =>
+  //           element['id'] ==
+  //           'https://data.geopf.fr/telechargement/resource/'.concat(produit)
+  //       )
+  //     ),
+  //     tap((el) => console.log(el)),
+  //     switchMap((buckets: Array<FormatProduit>) => {
+  //       const bucketPromises = [{
+  //         value: buckets[0][param],
+  //         label: buckets[0][param] || param,
+  //       }]
+  //       //console.log('bucket', bucketPromises)
+  //       return Promise.all(bucketPromises)
+  //     })
+  //   )
+  // }
 }
--- libs/feature/record/src/lib/ign-api-produit/ign-api-produit.component.ts
+++ libs/feature/record/src/lib/ign-api-produit/ign-api-produit.component.ts
@@ -1,60 +1,34 @@
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   Input,
   OnInit,
+  Output,
 } from '@angular/core'
-import { Observable, map, tap } from 'rxjs'
+import { DatasetDistribution } from '@geonetwork-ui/common/domain/model/record'
+import { Observable, catchError, map, tap, throwError } from 'rxjs'
 
 @Component({
   selector: 'gn-ui-ign-api-produit',
   templateUrl: './ign-api-produit.component.html',
   styleUrls: ['./ign-api-produit.component.css'],
 })
-export class IgnApiProduitComponent {
-  @Input() urlProduit
-
-  listeProduit: Observable<any>
+export class IgnApiProduitComponent implements OnInit {
+  @Input() link
+  @Input() color: string
+  @Input() format: string
+  @Input() isFromWfs: boolean
 
   constructor(protected http: HttpClient) {}
+  liste$: Observable<any>
 
-  ngOnInit() {
-    this.listeProduit = this.http
-      .get(this.urlProduit)
-      .pipe(map((reponse) => reponse['entry']))
+  ngOnInit(): void {
+    // console.log(this.link['id'])
+    this.liste$ = this.http.get(this.link['id']).pipe(
+      map((response) => response['entry'][0]['id'])
+      // tap((el) => console.log(el))
+    )
   }
-
-  // downloadFile() {
-  //   console.log('Telechargement!!')
-  //   this.listeProduit.subscribe((reponse) =>
-  //     reponse.map((el) => this.downloadProduct(el['id']))
-  //   )
-  // }
-
-  // downloadProduct(url) {
-  //   console.log(url)
-  // }
-
-  // (value: Array<any>) {
-  //   this.setProduits(value)
-  // }
-
-  // setProduits(value) {
-  //   console.log('r', value)
-  // }
-
-  // showProduits(value) {
-  //   console.log('y', value)
-  // }
-
-  // trackById(index: number, obj: any): number {
-  //   console.log(obj.id)
-  //   return obj.id
-  // }
-  // get getProduits() {
-  //   console.log('r', this.produits)
-
-  //   return this.produits ? this.produits : null
-  // }
 }
