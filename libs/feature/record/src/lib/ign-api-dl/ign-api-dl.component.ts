264578b2c60b35b3f94cf6818453dece9e7f9d72
Ronan <rfouille93@gmail.com>
2024-06-25 11:44:20 +0200

    fix(formapi): Ajustement de la réinitialisation de la page

--- libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
+++ libs/feature/record/src/lib/ign-api-dl/ign-api-dl.component.ts
@@ -191,6 +191,8 @@ export class IgnApiDlComponent implements OnInit {
   }
   resetPage(): void {
     this.page$.next('0')
+    this.pageSize$.next(this.initialPageSize)
+    this.size$.next(this.initialPageSize)
   }
 
   async getFields() {
