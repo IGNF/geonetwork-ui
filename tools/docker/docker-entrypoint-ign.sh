e665299ccd2bb6b8bdfd575398439ccaf23dbbc2
cde-barros <Christopher.De-Barros@ign.fr>
2024-09-03 16:50:38 +0200

    Configuration API env

--- tools/docker/docker-entrypoint-ign.sh
+++ tools/docker/docker-entrypoint-ign.sh
@@ -8,7 +8,8 @@ CONFIG_FILE_NAME=default.toml
 if [ ! -z "${GN4_API_URL}" ]
 then
   echo "[INFO] Replacing GN4 url in conf with: ${GN4_API_URL}..."
-  sed -i "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
+  sed "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME} > /tmp/${CONFIG_FILE_NAME} && cat /tmp/${CONFIG_FILE_NAME} > ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
+  rm /tmp/${CONFIG_FILE_NAME}
 fi
 
 echo "[INFO] docker-entrypoint-ign.sh ended successfully."
