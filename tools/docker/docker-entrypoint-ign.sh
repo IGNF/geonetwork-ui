eb12b4c389681424a2f2161cc64670fd8171e4e2
cde-barros <Christopher.De-Barros@ign.fr>
2024-09-05 11:17:35 +0200

    test parametrage datahub

--- tools/docker/docker-entrypoint-ign.sh
+++ tools/docker/docker-entrypoint-ign.sh
@@ -8,11 +8,9 @@ CONFIG_FILE_NAME=default.toml
 if [ ! -z "${GN4_API_URL}" ]
 then
   echo "[INFO] Replacing GN4 url in conf with: ${GN4_API_URL}..."
-  chmod 775 ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
   sed "s%geonetwork4_api_url = \".*\"%geonetwork4_api_url = \"${GN4_API_URL}\"%" ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME} > /tmp/${CONFIG_FILE_NAME} 
   rm -f ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
   cp /tmp/${CONFIG_FILE_NAME} ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
-  chmod 444 ${APP_FILES_PATH}${CONFIG_FILE_PATH}${CONFIG_FILE_NAME}
   rm -f /tmp/${CONFIG_FILE_NAME}
 fi
 
